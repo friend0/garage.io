d47e4c29f0a316c3c7e2a019df72490605cc0e9b
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2017-12-26 13:58:57 -0800

    adds hardware file

--- server/hardware.js
+++ server/hardware.js
@@ -1,1 +1,19 @@
-hardware.js
+// allows local dev on mac
+let Gpio;
+try {
+	Gpio = require('onoff').Gpio;
+} catch (e) {
+	console.log(e);
+}
+
+let controlPin;
+let ledPin;
+if (Gpio) {
+	controlPin = new Gpio(14, 'out');
+	ledPin = new Gpio(15, 'out');
+}
+
+module.exports = {
+	controlPin,
+	ledPin
+};
