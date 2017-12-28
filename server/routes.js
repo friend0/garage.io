f8b8663274ac56d8495c816750025deb2e8ced1c
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2017-12-27 21:01:33 -0800

    wip

--- server/routes.js
+++ server/routes.js
@@ -1,80 +1,78 @@
-const db = require('./db');
-const auth = require('./auth');
-const hardware = require('./hardware');
-const express = require('express');
+const db = require("./db");
+const auth = require("./auth");
+const hardware = require("./hardware");
+const express = require("express");
 const router = express.Router();
 
-const winston = require('winston');
+const winston = require("winston");
 
 const logger = winston.createLogger({
     json: true,
     colorize: true,
-  transports: [
-    new winston.transports.Console(),
-  ]
+    transports: [new winston.transports.Console()]
 });
 
+const asyncMiddleware = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
 
-const asyncMiddleware = fn =>
-    (req, res, next) => {
-        Promise.resolve(fn(req, res, next))
-            .catch(next);
-    };
-
-const loggerMiddleware =
-    (req, res, next) => {
-        logger.info({ method: req.method, time: Date.now()});
-        next();
-    };
+const loggerMiddleware = (req, res, next) => {
+    logger.info({ method: req.method, time: Date.now() });
+    next();
+};
 
 router.use(loggerMiddleware);
 
-router.get('/api/control', asyncMiddleware(async (req, res) => {
-    try {
-        await auth.authorizeWrite({
-            email: req.query.email,
-            password: req.query.password,
-        });
+router.get(
+    "/api/control",
+    asyncMiddleware(async (req, res) => {
+        try {
+            await auth.authorizeWrite({
+                email: req.query.email,
+                password: req.query.password
+            });
 
-        if (hardware.controlPin) {
-            hardware.controlPin.writeSync(1);
-            hardware.ledPin.writeSync(1);
-            setTimeout(() => {
-                hardware.ledPin.writeSync(0);
-                hardware.controlPin.writeSync(0);
-            }, 500);
+            if (hardware.controlPin) {
+                hardware.controlPin.writeSync(1);
+                hardware.ledPin.writeSync(1);
+                setTimeout(() => {
+                    hardware.ledPin.writeSync(0);
+                    hardware.controlPin.writeSync(0);
+                }, 500);
+            }
+            res.json({
+                message: "Authenticated.",
+                status: 200
+            });
+        } catch (e) {
+            console.log("There was an error handling this request", e);
+            res.json({
+                message: "There was an error handling this request.",
+                status: 400
+            });
         }
-        res.json({
-            message: 'Authenticated.',
-            status: 200
-        });
+    })
+);
 
-    } catch (e) {
-        console.log('There was an error handling this request', e)
-        res.json({
-            message: 'There was an error handling this request.',
-            status: 400
-        })
-    }
-}));
-
-
-router.post('/api/users', asyncMiddleware(async (req, res, next) => {
-    /*
+router.post(
+    "/api/users",
+    asyncMiddleware(async (req, res, next) => {
+        /*
       if there is an error thrown in getUserFromDb, asyncMiddleware
       will pass it to next() and express will handle the error;
     */
-    const insertResults = await db('users').insert({email: req.body.email, password: req.body.password}).returning('*');
-    // logger.info(insertResults)
-    res.json({ status: 200 });
-}));
+        const insertResults = await db("users")
+            .insert({ email: req.body.email, password: req.body.password })
+            .returning("*");
+        // logger.info(insertResults)
+        res.json({ status: 200 });
+    })
+);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-router.get('*', async (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+router.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname + "/../client/build"));
 });
-
-
 
 module.exports = router;
